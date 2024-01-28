07c607d68d30a3e82627ec3a9fd5d26f162ddba1
Helano Portela <helano38@gmail.com>
2024-01-28 17:59:58 -0300

    removing waitress server

--- main.py
+++ main.py
@@ -35,4 +35,4 @@ def extract_text():
         return jsonify({'error': 'Unsupported file format'}), 400
 
 if __name__ == '__main__':
-    app.run(port=8080)
+    serve(app1.app, host='0.0.0.0', port=8080)
